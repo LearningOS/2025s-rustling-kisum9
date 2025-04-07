c438f85f4ab61d43de3d5c499b1528b69b63167a
kisum9 <kisumtan93@gmail.com>
2025-04-07 10:09:47 +0800

    Finished all exercises

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,26 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        let args: Vec<&str> = s.split(",").collect();
+
+        if let [name, age] = args.as_slice() {
+            match name {
+                val if val.is_empty() => return Person::default(),
+                name => match age.parse::<usize>() {
+                    Ok(age) => {
+                        return Person {
+                            name: name.to_string(),
+                            age,
+                        }
+                    }
+                    Err(_) => return Person::default(),
+                },
+            }
+        } else {
+            return Person::default();
+        };
     }
 }
 
